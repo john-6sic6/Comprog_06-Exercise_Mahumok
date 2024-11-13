e709d470eef7720c2734fba5413688c00306816e
john-6sic6 <jonroidomafumoku@gmail.com>
2024-11-13 09:29:35 +0800

    Add files via upload

---
+++ Mahumok_comprog1.java
@@ -0,0 +1,22 @@
+package mahumok_comprog1;
+
+
+public class Mahumok_comprog1 {
+
+    public static void main(String[] args) {
+        
+       int iNumber = 3;
+       
+      if (iNumber >= 0) {
+      if (iNumber == 0) {
+        System.out.println("first string");
+    } else {
+        System.out.println("second string");
+    }
+    System.out.println("third string");
+}
+
+        }
+    }
+    
+
